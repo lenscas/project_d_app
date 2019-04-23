a3abb792e71482d8d909ebc5ab7512ba88091ae9
lenscas <lenscas@gmail.com>
2019-04-23 13:07:48 +0200

    Added way to go to page

--- www/js/index.js
+++ www/js/index.js
@@ -32,13 +32,7 @@ var app = {
 
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
+        go_to("test1")
         console.log('Received Event: ' + id);
     }
 };
