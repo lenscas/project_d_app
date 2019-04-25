8dd87ccbb737059cfd1a110013d8b8cd856bb5f2
lenscas <lenscas@gmail.com>
2019-04-25 13:20:36 +0200

    Add easy method to comunicate with speaker

---
+++ www/js/api.js
@@ -0,0 +1,34 @@
+API = (function(){
+	const addMinimal = async (url,data)=>{
+		data = data || {}
+		const res = await fetch(config.speaker_url + "/"+ url, {
+			...data,
+			mode : "cors",
+			headers : {
+				"Content-Type": "application/json"
+			},
+		})
+		return await res.json()
+	}
+	return{
+		get(url) {
+			return addMinimal(url)
+		},
+		post(url, data){
+			return addMinimal(url,{
+				method :"POST",
+				body: JSON.stringify(data)
+			})
+			return res.json()
+		},
+		put(url,data) {
+			return addMinimal(url,{
+				method : "PUT",
+				body: JSON.stringify(data)
+			})
+		},
+		delete(url) {
+			return addMinimal(url,{method:"DELETE"})
+		}
+	}
+})()
---
+++ www/js/config.js
@@ -0,0 +1,3 @@
+config = {
+	speaker_url : "http://127.0.0.1:3000"
+}
